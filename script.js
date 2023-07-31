e8837b66c07d33162133fa06bae159c1e3d4fa38
Yuvraj <arorayuvraj24@gmail.com>
2023-07-30 19:36:56 -0500

    test

--- script.js
+++ script.js
@@ -7,5 +7,9 @@ document.getElementById('button').addEventListener('click', buttonClicked);
 function buttonClicked() {
     //set heading by asking prompt
     let name = prompt('What is your name?');
+    if (name == null || name == '') {
+        
+    } else {
     document.getElementById('heading').textContent = 'Hello ' + name + '!';
+    }
 }
